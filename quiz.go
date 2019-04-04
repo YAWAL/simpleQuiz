942a427afb05251a29186d210957c6e8fe0e21de
YAWAL <yavorskyyval@gmail.com>
2019-04-05 00:51:36 +0300

    update main function

--- quiz.go
+++ quiz.go
@@ -1,67 +1,36 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
 
-	"github.com/gorilla/mux"
+	"github.com/YAWAL/simpleQuiz/config"
+	"github.com/YAWAL/simpleQuiz/domain"
+	"github.com/YAWAL/simpleQuiz/router"
 )
 
-// Question is a structure which represents info about one question in a quiz
-type Question struct {
-	Question      string   `json:"question,omitempty"`
-	Answers       *Answers `json:"answers,omitempty"`
-	CorrectAnswer string   `json:"correctAnswer,omitempty"`
-}
-
-// Answers is a structure which holds all answers related to question
-type Answers struct {
-	A string `json:"a,omitempty"`
-	B string `json:"b,omitempty"`
-	C string `json:"c,omitempty"`
-	D string `json:"d,omitempty"`
-}
-
-var quiz []Question
-
-func quizHandler(w http.ResponseWriter, req *http.Request) {
-	enableCORS(&w)
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(quiz)
-}
-
-func startQuiz() {
-	quiz = collectQuestions()
-
-	router := mux.NewRouter()
-	router.HandleFunc("/quiz", quizHandler).Methods(http.MethodGet)
-
-	log.Fatal(http.ListenAndServe(":8888", router))
-}
-
-// enable cross-origin resource sharing
-func enableCORS(w *http.ResponseWriter) {
-	(*w).Header().Set("Access-Control-Allow-Origin", "*")
-}
-
-func collectQuestions() []Question {
-	return []Question{
-		{Question: "What is the capital of the Malta?",
-			Answers:       &Answers{A: "Milan", B: "Valletta", C: "London", D: "Paris"},
-			CorrectAnswer: "b"},
-		{Question: "Who was the first chess champion?",
-			Answers:       &Answers{A: "Michail Tall", B: "Johannes Zukertort", C: "Wilhelm Steinitz", D: "Volodymyr Klitchko"},
-			CorrectAnswer: "c"},
-		{Question: "When Swedish became Sweden's official language?",
-			Answers:       &Answers{A: "1 June 2008", B: "1 June 2009", C: "1 July 2007", D: "1 July 2009"},
-			CorrectAnswer: "d"},
-		{Question: "What is not an OOP principle?",
-			Answers:       &Answers{A: "inheritance", B: "encapsulation", C: "polyglot", D: "abstraction"},
-			CorrectAnswer: "c"},
-	}
-}
+const configFile = "config.json"
 
 func main() {
-	startQuiz()
+	//read config
+	cnf, err := config.Load(configFile)
+	if err != nil {
+		log.Printf("can not load config from file: %s , err: %s", configFile, err)
+	}
+	log.Println("config loaded")
+
+	// from requirement: Database - Just in-memory , so no database
+	// init in-memory storage for quiz
+	players, err := domain.InitQuiz(cnf.QuestionAnswersFilePath)
+	if err != nil {
+		log.Printf("can not init quiz: %s", err)
+	}
+	log.Println("quiz initialized")
+
+	// init router
+	r := router.InitRouter(cnf.QuestionFilePath, players)
+	log.Printf("router initialized on port: %s", cnf.Port)
+
+	log.Fatal(http.ListenAndServe(cnf.Port, r))
+
 }
